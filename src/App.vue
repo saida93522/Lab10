461de979330860509dfc439e866857c3787b111c
SAIDA HUSSEN <ct8357ln@go.minneapolis.edu>
2021-04-25 16:40:39 -0500

    student sign in components app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <new-student-form v-on:student-added="newStudentAdded"></new-student-form>
+    <student-table
+      v-bind:students="students"
+      v-on:student-present="studentArrivedOrLeft"
+      v-on:delete-student="studentDeleted"
+    ></student-table>
+    <student-message v-bind:student="mostRecentStudent"> </student-message>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from "./components/NewStudentForm.vue";
+import StudentTable from "./components/StudentTable.vue";
+import StudentMessage from "./components/StudentMessage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  emits: ["student-added"], // Document the events this component emits
   components: {
-    HelloWorld
-  }
-}
+    NewStudentForm,
+    StudentMessage,
+    StudentTable,
+  },
+  data() {
+    return {
+      students: [],
+      mostRecentStudent: {},
+    };
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student);
+      this.students.sort(function (s1, s2) {
+        return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1;
+      });
+    },
+    studentArrivedOrLeft(student, present) {
+      let updateStudent = this.students.find((s) => {
+        if (s.name == student.name && s.starID == student.starID) {
+          return true;
+        }
+      });
+      if (updateStudent) {
+        updateStudent.present = present;
+        this.mostRecentStudent = student;
+      }
+    },
+    studentDeleted(student) {
+      this.students = this.students.filter((s) => {
+        s != student;
+      });
+      this.mostRecentStudent = {}; //clear message
+    },
+  },
+};
+
+//
 </script>
 
 <style>
+@import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
